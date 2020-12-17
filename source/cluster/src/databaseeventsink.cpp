9a7787f941fbe6b1541f21755575e3281c0e9495
hangeza <zaunick@exp2.physik.uni-giessen.de>
2020-12-17 22:14:59 +0100

    added calculation of coinc_time and cluster_coinc_time in DatabaseEventSink::process()

--- source/cluster/src/databaseeventsink.cpp
+++ source/cluster/src/databaseeventsink.cpp
@@ -27,7 +27,7 @@ void DatabaseEventSink::process(Event event)
     for (auto& evt: event.events()) {
         DbEntry entry { "L1Event" };
         // timestamp
-        entry.timestamp()=std::to_string(evt.start());
+        entry.timestamp()=std::to_string(event.epoch()*1e9+event.start());
         // tags
         entry.tags().push_back(std::make_pair("user", evt.data().user));
         entry.tags().push_back(std::make_pair("detector", evt.data().station_id));
@@ -41,19 +41,19 @@ void DatabaseEventSink::process(Event event)
         entry.fields().push_back(std::make_pair("coinc_level", std::to_string(event.n())));
         entry.fields().push_back(std::make_pair("counter", std::to_string(evt.data().ublox_counter)));
         entry.fields().push_back(std::make_pair("length", std::to_string(evt.duration())));
-
-        /*
-         * TODO: Implement calculation of time differences to first event (evt_coinc_time)
-         * and storage of the coincidence span (diff btw. first to last ts)
-         */
-
-        std::uint64_t evt_coinc_time = 0;
-        std::uint64_t cluster_coinc_time = 0;
-        entry.fields().push_back(std::make_pair("coinc_time", std::to_string(evt_coinc_time)));
-        entry.fields().push_back(std::make_pair("cluster_coinc_time", std::to_string(cluster_coinc_time)));
-        entry.fields().push_back(std::make_pair("time_ref", std::to_string(evt.data().gnss_time_grid)));
-        entry.fields().push_back(std::make_pair("valid_fix", std::to_string(evt.data().fix)));
-
+        
+		/*
+		 * TODO: Implement calculation of time differences to first event (evt_coinc_time)
+		 * and storage of the coincidence span (diff btw. first to last ts)
+		 */
+		
+		std::int64_t evt_coinc_time = (evt.epoch()-event.epoch())*1e9 + (evt.start()-event.start());
+        std::int64_t cluster_coinc_time = evt.end()-evt.start();
+		entry.fields().push_back(std::make_pair("coinc_time", std::to_string(evt_coinc_time)));
+		entry.fields().push_back(std::make_pair("cluster_coinc_time", std::to_string(cluster_coinc_time)));
+        entry.fields().push_back(std::make_pair("time_ref", std::to_string((int)evt.data().gnss_time_grid)));
+        entry.fields().push_back(std::make_pair("valid_fix", std::to_string((int)evt.data().fix)));
+		
         if (m_link->write_entry(entry)) {
             return;
         }

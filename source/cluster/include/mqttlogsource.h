9b6181be876e56025f021d2a66c22a173800e9c9
hangeza <zaunick@exp2.physik.uni-giessen.de>
2020-12-20 00:12:01 +0100

    renamed class DetectorLog to DetectorInfo since the latter reflects better the incoming information about a detector by a mqtt daemon log or otherwise. The name DetectorLog thus now could be used for a class which represents log data collected by the cluster prog and sent out on different ways, currently MQTT and InfluxDB links.

--- source/cluster/include/mqttlogsource.h
+++ source/cluster/include/mqttlogsource.h
@@ -11,7 +11,7 @@
 
 namespace MuonPi {
 
-class DetectorLog;
+class DetectorInfo;
 class MessageParser;
 
 struct DetectorLogItem {
@@ -60,7 +60,7 @@ struct PartialLogEntry {
 /**
  * @brief The MqttLogSource class
  */
-class MqttLogSource : public AbstractSource<DetectorLog>
+class MqttLogSource : public AbstractSource<DetectorInfo>
 {
 public:
     /**

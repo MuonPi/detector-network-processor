01ad1ba344379db6a59a3293e24e4cb7415fbe96
Daniel Treffenstädt <daniel@treffenstaedt.de>
2021-01-01 21:55:03 +0100

    Changed main.cpp to not use the ascii sinks for when the code is run as a system service

--- src/main.cpp
+++ src/main.cpp
@@ -54,11 +54,6 @@ auto main() -> int
         return -1;
     }
 
-
-    MuonPi::Sink::Ascii<MuonPi::ClusterLog> ascii_clusterlog_sink { std::cout };
-    MuonPi::Sink::Ascii<MuonPi::DetectorSummary> ascii_detectorlog_sink { std::cout };
-    MuonPi::Sink::Ascii<MuonPi::Event> ascii_event_sink { std::cout };
-
 #ifdef CLUSTER_RUN_SERVER
     MuonPi::Link::Database db_link {MuonPi::Config::influx.host, {MuonPi::Config::influx.login.username, MuonPi::Config::influx.login.password}, MuonPi::Config::influx.database};
 
@@ -77,15 +72,25 @@ auto main() -> int
     MuonPi::Sink::Mqtt<MuonPi::Event> mqtt_broadcast_sink { mqtt_link.publish("muonpi/events") };
 
 
+#ifndef CLUSTER_RUN_SERVICE
+    MuonPi::Sink::Ascii<MuonPi::ClusterLog> ascii_clusterlog_sink { std::cout };
+    MuonPi::Sink::Ascii<MuonPi::DetectorSummary> ascii_detectorlog_sink { std::cout };
+    MuonPi::Sink::Ascii<MuonPi::Event> ascii_event_sink { std::cout };
+
     MuonPi::Sink::Collection<MuonPi::ClusterLog, 2> cluster_sinks {{&ascii_clusterlog_sink, &clusterlog_sink}};
     MuonPi::Sink::Collection<MuonPi::DetectorSummary, 2> detector_sinks {{&ascii_detectorlog_sink, &detectorlog_sink}};
-    MuonPi::Sink::Collection<MuonPi::Event, 2> event_sinks {{&ascii_event_sink, &event_sink/*, &mqtt_broadcast_sink*/}};
+    MuonPi::Sink::Collection<MuonPi::Event, 3> event_sinks {{&ascii_event_sink, &event_sink, &mqtt_broadcast_sink}};
+#else
+    MuonPi::Sink::Collection<MuonPi::ClusterLog, 1> cluster_sinks {{&clusterlog_sink}};
+    MuonPi::Sink::Collection<MuonPi::DetectorSummary, 1> detector_sinks {{&detectorlog_sink}};
+    MuonPi::Sink::Collection<MuonPi::Event, 2> event_sinks {{&event_sink, &mqtt_broadcast_sink}};
+#endif
+
 
     MuonPi::StateSupervisor supervisor{cluster_sinks};
     MuonPi::DetectorTracker detector_tracker{detector_sinks, supervisor};
     MuonPi::Core core{event_sinks, detector_tracker, supervisor};
 
-    MuonPi::Sink::Collection<MuonPi::Event, 2> incoming_sinks {{&core, &event_sinks}};
 
     MuonPi::Source::Mqtt<MuonPi::Event> event_source { core, mqtt_link.subscribe("muonpi/data/#") };
     MuonPi::Source::Mqtt<MuonPi::DetectorInfo> log_source { detector_tracker, mqtt_link.subscribe("muonpi/log/#") };
@@ -95,7 +100,6 @@ auto main() -> int
     supervisor.add_thread(&event_sinks);
     supervisor.add_thread(&detector_sinks);
     supervisor.add_thread(&cluster_sinks);
-    supervisor.add_thread(&incoming_sinks);
 
 
     shutdown_handler = [&](int signal) {

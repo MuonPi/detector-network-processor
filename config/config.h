859abc3c42ce37352934b208397364abb228619c
Daniel Treffenstädt <daniel@treffenstaedt.de>
2021-03-30 20:32:37 +0200

    Added libraries to target link

--- config/config.h
+++ config/config.h
@@ -54,6 +54,7 @@ struct Trigger {
 
 struct Rest {
     int port {};
+    std::string address {};
     std::string cert {};
     std::string privkey {};
     std::string fullchain {};
@@ -75,7 +76,7 @@ static ConfigFiles files {"/etc/muondetector/muondetector-cluster.cfg", "/var/mu
 static Mqtt mqtt{"", 1883, {}};
 static Influx influx{"", {"", ""}, "", ""};
 static Ldap ldap{"ldaps://muonpi.org", {"", ""}};
-static Rest rest{1983, "file://", "file://", "file://"};
+static Rest rest{1983, "", "file://", "file://", "file://"};
 static Trigger trigger{"/var/muondetector/cluster_trigger"};
 static Interval interval {std::chrono::seconds{60}, std::chrono::seconds{120}};
 static Meta meta {false, 6};

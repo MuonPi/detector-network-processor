aec3522b7f1d8c6ee180a38173e10bc3969f06c4
Daniel Treffenstädt <daniel@treffenstaedt.de>
2021-03-30 17:24:51 +0200

    Added base64 header

---
+++ include/utility/base64.h
@@ -0,0 +1,28 @@
+#ifndef BASE64_H
+#define BASE64_H
+
+#include <boost/archive/iterators/binary_from_base64.hpp>
+#include <boost/archive/iterators/base64_from_binary.hpp>
+#include <boost/archive/iterators/transform_width.hpp>
+#include <boost/algorithm/string.hpp>
+
+namespace MuonPi::base64 {
+
+[[nodiscard]] inline auto decode(std::string_view val) -> std::string {
+    using namespace boost::archive::iterators;
+    using It = transform_width<binary_from_base64<std::string::const_iterator>, 8, 6>;
+    return boost::algorithm::trim_right_copy_if(std::string(It(std::begin(val)), It(std::end(val))), [](char c) {
+        return c == '\0';
+    });
+}
+
+[[nodiscard]] inline auto encode(std::string_view val) -> std::string {
+    using namespace boost::archive::iterators;
+    using It = base64_from_binary<transform_width<std::string::const_iterator, 6, 8>>;
+    auto tmp = std::string(It(std::begin(val)), It(std::end(val)));
+    return tmp.append((3 - val.size() % 3) % 3, '=');
+}
+
+}
+
+#endif // BASE64_H

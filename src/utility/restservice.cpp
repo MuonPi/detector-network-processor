28fc26a548d53c878f5dd08c51a52ae0d8bd7e2c
Daniel Treffenstädt <daniel@treffenstaedt.de>
2021-04-01 13:38:22 +0200

    Improved http status replies for rest service

--- src/utility/restservice.cpp
+++ src/utility/restservice.cpp
@@ -261,7 +261,7 @@ void service::on_stop()
 auto service::handle(request_type req) const -> response_type
 {
     if (req.target().empty() || req.target()[0] != '/' || (req.target().find("..") != beast::string_view::npos)) {
-        return http_response<http::status::bad_request>(req, "Illegal request-target");
+        return http_response<http::status::bad_request>(req, "Malformed request-target");
     }
     if (m_handler.empty()) {
         return http_response<http::status::service_unavailable>(req, "No handler installed");
@@ -285,7 +285,7 @@ auto service::handle(request_type req, std::queue<std::string> path, const std::
     }
 
     if (path.empty()) {
-        return http_response<http::status::bad_request>(req, "Illegal request-target");
+        return http_response<http::status::bad_request>(req, "Request-target empty");
     }
 
     auto it = handlers.begin();
@@ -306,7 +306,7 @@ auto service::handle(request_type req, std::queue<std::string> path, const std::
         std::string auth { req[http::field::authorization] };
 
         if (auth.empty()) {
-            return http_response<http::status::unauthorized>(req, "Unauthorised");
+            return http_response<http::status::unauthorized>(req, "Need authorisation");
         }
         constexpr std::size_t header_length { 6 };
 
@@ -317,7 +317,7 @@ auto service::handle(request_type req, std::queue<std::string> path, const std::
         auto password = auth.substr(delimiter + 1);
 
         if (!hand.authenticate(request { req }, username, password)) {
-            return http_response<http::status::unauthorized>(req, "Unauthorised");
+            return http_response<http::status::unauthorized>(req, "Authorisation failed for user: '" + username + "'");
         }
     }
 
